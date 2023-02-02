fb7269f516f5279b2d9df5f21390d4eb9b92c957
vladimiremi <vladimiremi2014@gmail.com>
2023-02-02 16:46:59 -0300

    refactor: adicionando totais de tarefas e seus estilos

--- toDoList-app/src/components/Label/index.tsx
+++ toDoList-app/src/components/Label/index.tsx
@@ -1,11 +1,19 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { styles } from './styles';
 
 interface LabelProps {
   color: string;
   children: string;
+  total: number;
 }
 
-export function Label({ color, children }: LabelProps) {
-  return <Text style={[styles.label, { color: color }]}>{children}</Text>;
+export function Label({ color, children, total }: LabelProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.label, { color: color }]}>{children}</Text>
+      <View style={styles.containerTotal}>
+        <Text style={styles.textTotal}>{total}</Text>
+      </View>
+    </View>
+  );
 }
--- toDoList-app/src/components/Label/styles.ts
+++ toDoList-app/src/components/Label/styles.ts
@@ -1,8 +1,26 @@
 import { StyleSheet } from 'react-native';
+import { colors } from '../../configs/colors';
 
 export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
   label: {
     fontSize: 14,
     fontWeight: '700',
+  },
+  containerTotal: {
+    backgroundColor: colors.gray400,
+    borderRadius: 999,
+    width: 25,
+    height: 19,
+    alignItems: 'center',
+    marginLeft: 5,
+    justifyContent: 'center',
+  },
+  textTotal: {
+    color: colors.gray200,
+    fontWeight: '700',
+    fontSize: 12,
   },
 });
